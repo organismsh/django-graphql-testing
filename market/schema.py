efc648d0fb26a04d5d3b101cdf28342a693a9da4
organism.sh <organism.sh@gmail.com>
2020-05-22 03:55:10 +0530

    screenshots of query

--- market/schema.py
+++ market/schema.py
@@ -15,12 +15,16 @@ class Carts(DjangoObjectType):
 class Query(graphene.ObjectType):
     products = graphene.List(Product)
     cart = graphene.List(Carts)
+    product = graphene.Field(Product, product_id=graphene.String())
 
     def resolve_products(self, info):
         return Products.objects.all()
     
     def resolve_cart(self, info):
         return Cart.objects.all()
+
+    def resolve_product(self, info, product_id):
+        return Products.objects.get(pk=product_id)
  
     
 schema = graphene.Schema(query=Query)
